8e00246ed7c9adb2ac27cab9add85b2cc6140a1c
Nikki <piknik@me.com>
2021-07-06 07:22:33 -0600

    Fix some spacing but still not happy with the textareas

--- src/views/InterviewForm.vue
+++ src/views/InterviewForm.vue
@@ -5,19 +5,23 @@
       <form v-on:submit.prevent.self="onSubmit">
         <fieldset>
           <legend>Submit a new interview experience</legend>
-          <label for="company">Company: </label>
+          <label for="user">Your name:<sup class="required">*</sup> </label>
+          <input v-model="formData.user" id="user" type="text" placeholder="First name and last initial, if you don't mind" required>
+          <br>
+
+          <label for="company">Company:<sup class="required">*</sup> </label>
           <input v-model="formData.company" id="company" type="text" placeholder="Company Name" required>
           <br>
 
           <label for="offer">Offer received: </label>
-          <input v-model="formData.offer" id="offer" type="checkbox" required>
+          <input v-model="formData.offer" id="offer" type="checkbox">
           <br>
 
-          <label for="date">Date you applied: </label>
+          <label for="date">Date you applied:<sup class="required">*</sup> </label>
           <input v-model="formData.date" id="date" type="date" required>
           <br>
 
-          <label>Steps (select all that apply, at least one)</label>
+          <label>Steps (select all that apply, at least one)<sup class="required">*</sup></label>
           <div class="options-wrapper">
             <div>
               <input type="checkbox" id="steps-screen" value="screening" v-model="formData.steps">
@@ -34,6 +38,10 @@
             <div>
               <input type="checkbox" id="steps-takehome" value="takehome" v-model="formData.steps">
               <label for="steps-takehome" class="step-options">Takehome Challenge</label>
+            </div>
+            <div>
+              <input type="checkbox" id="steps-tech" value="technical" v-model="formData.steps">
+              <label for="steps-tech" class="step-options">Technical interview</label>
             </div>
             <div>
               <input type="checkbox" id="steps-whiteboard" value="whiteboard" v-model="formData.steps">
@@ -56,7 +64,7 @@
                     id="description"
                     placeholder="As much detail as you can"
                     rows="2"
-                    cols="40"
+                    cols="60"
                     wrap="soft"
           ></textarea>
           <br>
@@ -66,7 +74,7 @@
                     id="solution"
                     placeholder="What did you do?"
                     rows="2"
-                    cols="40"
+                    cols="60"
                     wrap="soft"
           ></textarea>
           <br>
@@ -74,20 +82,21 @@
           <label for="retro">Retrospective: </label>
           <textarea v-model="formData.retro"
                     id="retro"
-                    placeholder="What could you have done better?"
+                    placeholder="What could you have done better? Include feedback, if you received it."
                     rows="2"
-                    cols="40"
+                    cols="60"
                     wrap="soft"
           ></textarea>
           <br>
 
-          <label for="takeaways">Takeaways: </label>
+          <label for="takeaways">Takeaways (tl;dr):<sup class="required">*</sup> </label>
           <textarea v-model="formData.takeaways"
                     id="takeaways"
                     placeholder="What lessons were learned?"
                     rows="2"
-                    cols="40"
+                    cols="60"
                     wrap="soft"
+                    required
           ></textarea>
           <br>
           <div class="button-wrapper">
@@ -95,13 +104,13 @@
           </div>
         </fieldset>
       </form>
-      <div class="form-data" v-if="formData.company">
+<!--      <div class="form-data" v-if="formData.company">-->
 <!--        <p>slug: {{ slug }}</p>-->
-        <p>company: {{ formData.company }}  ({{ formData.offer ? 'offer received' : 'no offer' }})</p>
-        <p>Applied {{ formData.date }}</p>
-        <p>{{ formData.description }}</p>
-        <p>steps {{ formData.steps }}</p>
-      </div>
+<!--        <p>company: {{ formData.company }}  ({{ formData.offer ? 'offer received' : 'no offer' }})</p>-->
+<!--        <p>Applied {{ formData.date }}</p>-->
+<!--        <p>{{ formData.description }}</p>-->
+<!--        <p>steps {{ formData.steps }}</p>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
@@ -115,6 +124,7 @@ export default {
   data () {
     return {
       formData: {
+        user: '',
         company: '',
         date: '',
         description: '',
@@ -151,6 +161,7 @@ export default {
       console.log(this.formData)
 
       return db.collection('interviews').add({
+        user: this.formData.user,
         company: this.formData.company,
         date: this.formData.date,
         description: this.formData.description,
@@ -174,18 +185,18 @@ export default {
 
 <style scoped>
 .block {
-  width: 80%;
+  /*width: 80%;*/
   margin: 20px auto;
   text-align: left;
   font-size: 1.6rem;
 }
 
-.form-wrapper {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: left;
-  align-items: flex-start;
-}
+/*.form-wrapper {*/
+/*  display: flex;*/
+/*  flex-flow: row nowrap;*/
+/*  justify-content: left;*/
+/*  align-items: flex-start;*/
+/*}*/
 
 form {
   width: 60%;
@@ -216,6 +227,8 @@ textarea {
   margin: 10px 20px;
   border-bottom: 1px solid grey;
   padding: 0 5px;
+  max-width: 600px;
+  max-height: 100%;
 }
 
 /*.form-data {*/
@@ -266,5 +279,9 @@ button {
   margin-right: 2rem;
   width: 100px;
   font-size: 1.2rem;
+}
+
+.required {
+  color: red;
 }
 </style>
