40cd4f22e77b62aee546232a6d3312c62b1c3900
Nikki <piknik@me.com>
2021-07-06 07:38:40 -0600

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Welcome to Launcher</h1>
+    <p>This app was created as a way for us to share our experiences in interviewing in the tech space. Interviews are hard
+    and it's even harder when you don't know what you're being judged on. If I can help launch your career by giving you the 411
+      on my interview experience, then I'm all about that (and I hope you are, too). So please share your experiences, and be honest. Share both
+    the happy and sad paths, please. We can all learn from your experiences.</p>
   </div>
 </template>
+
+<style scoped>
+.about {
+  width: 60%;
+  font-size: 1.3rem;
+  margin: 20px auto;
+  line-height: 2;
+}
+
+h1 {
+  margin: 20px;
+}
+</style>
